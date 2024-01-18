c579aa3e6dd0fbabb9d1286495b93a8f4b0c02df
Karn <supriya.karn@capgemini.com>
2024-01-18 15:53:18 +0530

    first commit

---
+++ ValidateUser.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace valiateuser
+{
+    class ValidateUser
+    {
+        string u;
+        string p;
+        public void GetDetails()
+        {
+            Console.WriteLine("Enter username: ");
+            u = Console.ReadLine();
+            Console.WriteLine("Enter password: ");
+            p = Console.ReadLine();
+        }
+        public bool ValidateDetails(string username,string password)
+        {
+            if (username == "supriya" && password =="123")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void DisplayDetails()
+        {
+            if (ValidateDetails(u, p))
+            {
+                Console.WriteLine("Login successful");
+            }
+            else
+            {
+                Console.WriteLine("Login failed");
+            }
+        }
+        
+
+    }
+}
